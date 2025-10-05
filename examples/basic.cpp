573ebb26e3c1b5fa2daae48bad6d14b823fe730c
Marco <180540729+msh31@users.noreply.github.com>
2025-10-05 23:47:48 +0200

    examples: add basic (wip)

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,7 +1,7 @@
 #include "mage/core/engine.hpp"
 
 int main() {
-    mage::Engine engine(800, 600, "MAGE Engine Test");
+    mage::Engine engine(GetScreenWidth(), GetScreenHeight(), "MAGE Engine Test");
     engine.run();
     return 0;
 }
